da8511b2a6740833e04f9d79cf5540c5c15e60be
Toshio Ito <debug.ito@gmail.com>
2014-02-06 22:01:55 +0900

    Add PrototypedData class. I think I'll need it for implementation. just a skeleton for now.

---
+++ lib/Gnuplot/Builder/PrototypedData.pm
@@ -0,0 +1,13 @@
+package Gnuplot::Builder::PrototypedData;
+use strict;
+use warnings;
+
+## Common data structure for Script and Dataset. It has a
+## PartiallyKeyedIndex and optional named attributes. It implements
+## prototype-based inheritance.
+
+## should it have setq()? or setq() can be implemented as a common wrapper function.
+
+## should it have a method to get normalized values (code-refs are evalulated)?
+
+1;
