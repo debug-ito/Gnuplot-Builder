c304455ec5a9d44b8ca761892476881247147cc2
Toshio Ito <debug.ito@gmail.com>
2014-08-17 12:07:07 +0900

    Release 0.22

--- lib/Gnuplot/Builder.pm
+++ lib/Gnuplot/Builder.pm
@@ -6,7 +6,7 @@ use Gnuplot::Builder::Script;
 use Gnuplot::Builder::Dataset;
 use Gnuplot::Builder::Process;
 
-our $VERSION = "0.21";
+our $VERSION = "0.22";
 
 our @EXPORT = our @EXPORT_OK = qw(gscript gfunc gfile gdata ghelp);
 
