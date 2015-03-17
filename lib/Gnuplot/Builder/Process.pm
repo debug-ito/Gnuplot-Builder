6371ccdd0a14218841b710f3590501966828f376
Toshio Ito <debug.ito@gmail.com>
2015-03-17 22:19:46 +0900

    Process: POD: add specification $ENCODING. add basic tests.

--- lib/Gnuplot/Builder/Process.pm
+++ lib/Gnuplot/Builder/Process.pm
@@ -261,6 +261,7 @@ Gnuplot::Builder::Process - gnuplot process manager
     use Gnuplot::Builder::Process;
     
     @Gnuplot::Builder::Process::COMMAND = ("/path/to/gnuplot", "-p");
+    $Gnuplot::Builder::Process::ENCODING = "utf8";
 
 =head1 DESCRIPTION
 
@@ -343,6 +344,17 @@ Example:
         my ($pid, $event, $body) = @_;
         warn "PID:$pid, EVENT:$event, BODY:$body";
     };
+
+=head2 $ENCODING
+
+If set, L<Gnuplot::Builder> encodes the script string in the specified encoding just before streaming into the gnuplot process.
+You can specify any encoding names recognizable by L<Encode> module.
+
+By default it's C<undef>, meaning it doesn't encode the script.
+
+You can also set this variable by the environment variable
+C<PERL_GNUPLOT_BUILDER_PROCESS_ENCODING>.
+
 
 =head1 AUTHOR
 
--- t/process/env.t
+++ t/process/env.t
@@ -5,6 +5,7 @@ BEGIN {
     $ENV{PERL_GNUPLOT_BUILDER_PROCESS_COMMAND} = "./hoge/gnuplot --persist";
     $ENV{PERL_GNUPLOT_BUILDER_PROCESS_MAX_PROCESSES} = 99999;
     $ENV{PERL_GNUPLOT_BUILDER_PROCESS_PAUSE_FINISH} = 100;
+    $ENV{PERL_GNUPLOT_BUILDER_PROCESS_ENCODING} = "shift_jis";
 }
 use Gnuplot::Builder::Process;
 
@@ -20,7 +21,9 @@ is $Gnuplot::Builder::Process::MAX_PROCESSES,
 is $Gnuplot::Builder::Process::PAUSE_FINISH,
     100,
     "PAUSE_FINISH is customized via env";
+
+is $Gnuplot::Builder::Process::ENCODING,
+    "shift_jis",
+    "ENCODING is customized via env";
     
 done_testing;
-
-
